bf5e88876bddb654de2b34cd1b81dcd13945e2fa
Vineet Yadav <mail@vineetyadav.in>
2022-02-23 04:09:32 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit test
+          Edit test 1
         </p>
         <a
           className="App-link"
