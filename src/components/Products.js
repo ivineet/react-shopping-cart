d275da1130aad745853a02c93e5e45f4d915e9ad
Vineet Yadav <mail@vineetyadav.in>
2022-02-24 03:55:26 +0530

    card-cmponent

--- src/components/Products.js
+++ src/components/Products.js
@@ -15,7 +15,7 @@ export default class Products extends Component{
 
                                 <div className="product-price">
                                     <div>{product.price}</div>
-                                    <button className="button primary">Add to Cart</button>
+                                    <button onClick={() => this.props.addToCart(product)} className="button primary">Add to Cart</button>
                                 </div>
                             </div>
                         </li>
